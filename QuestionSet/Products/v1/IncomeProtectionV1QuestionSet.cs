99176c1ad29e95dee9f9d637a10d6fa1f2f3a995
Moh Moh Oo <mmoo@shepherdsfriendly.co.uk>
2018-12-11 13:42:08 +0000

    Code formatting

--- QuestionSet/Products/v1/IncomeProtectionV1QuestionSet.cs
+++ QuestionSet/Products/v1/IncomeProtectionV1QuestionSet.cs
@@ -39,71 +39,71 @@ namespace QuestionSet.Products.v1
                             "Working on board sea or ocean going vessels"),
 
                 Question("Have you ever made an application to The Shepherds Friendly Society that has been postponed, declined, offered on special terms or cancelled?")
-                        .ValidationWarning("If yes, full underwriting",
-                            incomeProtection => incomeProtection.HasApplicationToShepherdsWithSpecifiedStatus
-                                ? ValidationResult.Underwriting
-                                : ValidationResult.Valid)
-                        .NoAdditionalStatement(),
+                    .ValidationWarning("If yes, full underwriting",
+                        incomeProtection => incomeProtection.HasApplicationToShepherdsWithSpecifiedStatus
+                            ? ValidationResult.Underwriting
+                            : ValidationResult.Valid)
+                    .NoAdditionalStatement(),
 
                 Question("Do any of the following statements apply to you?")
-                        .ValidationWarning("If yes, full underwriting",
-                            incomeProtection => incomeProtection.HasSpecifiedIllnessOrCondition
-                                ? ValidationResult.Underwriting
-                                : ValidationResult.Valid)
-                        .AvailableStatements("I am currently unable to work or am working reduced hours or on restricted duties due to sickness or accident.",
-                                "I have suffered from symptoms of chronic fatigue syndrome, ME or fibromyalgia in the last 3 years.",
-                                "I have suffered from cancer or malignant tumour which has been treated with radiotherapy or chemotherapy in the last 3 years.",
-                                "I am currently suffering from an illness for which I am being prescribed methotrexate or chemotherapy in the last 3 years.",
-                                "I am currently suffering from an illness for which I am being prescribed oral steroids or immunosuppressive treatment.",
-                                "I have had a Stroke or mini Stroke (also known as Transient Ischaemic attack)",
-                                "I have had a heart attack, or been diagnosed with angina or coronary disease.",
-                                "I have been diagnosed with a disease of the central nervous system such as multiple sclerosis, Parkinson’s disease, Alzheimer’s disease or dementia.",
-                                "I am suffering from paralysis, paraplegia or quadriplegia caused by damage to my spinal cord.",
-                                "I have suffered from or been diagnosed with diabetes (other than during pregnancy)",
-                                "I have been diagnosed with HIV or I am awaiting the results of a HIV test.",
-                                "I have undergone major organ transplant."),
+                    .ValidationWarning("If yes, full underwriting",
+                        incomeProtection => incomeProtection.HasSpecifiedIllnessOrCondition
+                            ? ValidationResult.Underwriting
+                            : ValidationResult.Valid)
+                    .AvailableStatements("I am currently unable to work or am working reduced hours or on restricted duties due to sickness or accident.",
+                            "I have suffered from symptoms of chronic fatigue syndrome, ME or fibromyalgia in the last 3 years.",
+                            "I have suffered from cancer or malignant tumour which has been treated with radiotherapy or chemotherapy in the last 3 years.",
+                            "I am currently suffering from an illness for which I am being prescribed methotrexate or chemotherapy in the last 3 years.",
+                            "I am currently suffering from an illness for which I am being prescribed oral steroids or immunosuppressive treatment.",
+                            "I have had a Stroke or mini Stroke (also known as Transient Ischaemic attack)",
+                            "I have had a heart attack, or been diagnosed with angina or coronary disease.",
+                            "I have been diagnosed with a disease of the central nervous system such as multiple sclerosis, Parkinson’s disease, Alzheimer’s disease or dementia.",
+                            "I am suffering from paralysis, paraplegia or quadriplegia caused by damage to my spinal cord.",
+                            "I have suffered from or been diagnosed with diabetes (other than during pregnancy)",
+                            "I have been diagnosed with HIV or I am awaiting the results of a HIV test.",
+                            "I have undergone major organ transplant."),
 
                 Question("Have you ever been referred to see a psychiatrist.")
-                        .ValidationWarning("If yes, full underwriting",
-                            incomeProtection => incomeProtection.HasBeenReferredToPsychiatrist
-                                ? ValidationResult.Underwriting
-                                : ValidationResult.Valid)
-                        .NoAdditionalStatement(),
+                    .ValidationWarning("If yes, full underwriting",
+                        incomeProtection => incomeProtection.HasBeenReferredToPsychiatrist
+                            ? ValidationResult.Underwriting
+                            : ValidationResult.Valid)
+                    .NoAdditionalStatement(),
 
                 Question("Height and Weight for BMI")
-                        .ValidationWarning("If >35, full underwriting",
-                            incomeProtection => incomeProtection.HeightInMeter / incomeProtection.WeightInKg > 35
-                                ? ValidationResult.Underwriting
-                                : ValidationResult.Valid)
-                        .NoAdditionalStatement(),
+                    .ValidationWarning("If >35, full underwriting",
+                        incomeProtection => incomeProtection.HeightInMeter / incomeProtection.WeightInKg > 35
+                            ? ValidationResult.Underwriting
+                            : ValidationResult.Valid)
+                    .NoAdditionalStatement(),
 
                 Question("How many cigarettes do you currently smoke per day?")
-                        .ValidationWarning("If >20pc, full underwriting",
-                            incomeProtection => incomeProtection.CigarettesPerDay > 20
-                                ? ValidationResult.Underwriting
-                                : ValidationResult.Valid)
-                        .NoAdditionalStatement(),
+                    .ValidationWarning("If >20pc, full underwriting",
+                        incomeProtection => incomeProtection.CigarettesPerDay > 20
+                            ? ValidationResult.Underwriting
+                            : ValidationResult.Valid)
+                    .NoAdditionalStatement(),
 
                 Question("Do you consume more than 30 units of alcohol per week or have you ever been dependant on alcohol or been advised to reduce your intake?")
-                        .ValidationWarning("If yes, full underwriting",
-                            incomeProtection => incomeProtection.HasOrHadAlcoholDependency
-                                ? ValidationResult.Underwriting
-                                : ValidationResult.Valid)
-                        .NoAdditionalStatement(),
+                    .ValidationWarning("If yes, full underwriting",
+                        incomeProtection => incomeProtection.HasOrHadAlcoholDependency
+                            ? ValidationResult.Underwriting
+                            : ValidationResult.Valid)
+                    .NoAdditionalStatement(),
 
                 Question("Have you used cannabis within the past year, or do you intend to begin using cannabis?")
-                        .ValidationWarning("If yes, full underwriting",
-                            incomeProtection => incomeProtection.HasUsedOrIntendToUseCannabis
-                                ? ValidationResult.Underwriting
-                                : ValidationResult.Valid)
-                        .NoAdditionalStatement(),
+                    .ValidationWarning("If yes, full underwriting",
+                        incomeProtection => incomeProtection.HasUsedOrIntendToUseCannabis
+                            ? ValidationResult.Underwriting
+                            : ValidationResult.Valid)
+                    .NoAdditionalStatement(),
 
                 Question("Other than cannabis, have you ever or do you intend to use any recreational or non prescription drugs (e.g. cannabis, ecstacy, cocaine, heroin, annabolic steroids etc?")
-                        .ValidationWarning("If yes, full underwriting",
-                            incomeProtection => incomeProtection.HasUsedOrIntendToUseAnyOtherReacreationalDrug
-                        ? ValidationResult.Underwriting
-                        : ValidationResult.Valid)
-                        .NoAdditionalStatement());
+                    .ValidationWarning("If yes, full underwriting",
+                        incomeProtection => incomeProtection.HasUsedOrIntendToUseAnyOtherReacreationalDrug
+                            ? ValidationResult.Underwriting
+                            : ValidationResult.Valid)
+                    .NoAdditionalStatement());
 
             return questionWithValidations.Item1;
 
