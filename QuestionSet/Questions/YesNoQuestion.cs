84fd1aed6791193a8e8e00487f77f696a9d3111d
Moh Moh Oo <mmoo@shepherdsfriendly.co.uk>
2018-12-11 11:04:37 +0000

    Renamed variables

--- QuestionSet/Questions/YesNoQuestion.cs
+++ QuestionSet/Questions/YesNoQuestion.cs
@@ -11,15 +11,15 @@
 
         public string ValidationDescription { get; }
 
-        public string[] Options { get; }
+        public string[] Statements { get; }
 
-        public YesNoQuestion(int id, string text, string validationDescription, params string[] options)
+        public YesNoQuestion(int id, string text, string validationDescription, params string[] availableStatements)
         {
             Id = id;
             Text = text;
             Type = "Question to select Yes or No";
             ValidationDescription = validationDescription;
-            Options = options;
+            Statements = availableStatements;
         }
     }
 }
